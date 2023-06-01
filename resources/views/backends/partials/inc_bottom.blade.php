26cf5d056d4398b524dd78150f425310e75d6daa
dachdalin <playboyfcteam177@gmail.com>
2023-06-02 00:57:57 +0700

    edit inc_file, route

--- resources/views/backends/partials/inc_bottom.blade.php
+++ resources/views/backends/partials/inc_bottom.blade.php
@@ -29,6 +29,6 @@
 <!-- AdminLTE App -->
 <script src="{{ asset('backends/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset('backends/dist/js/demo.js') }}"></script>
+{{-- <script src="{{ asset('backends/dist/js/demo.js') }}"></script> --}}
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('backends/dist/js/pages/dashboard.js') }}"></script>
