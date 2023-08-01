3f77ff836572a65ee0183bbba3a33228fce11552
dachdalin <playboyfcteam177@gmail.com>
2023-08-02 01:19:28 +0700

    edite profile

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -37,38 +37,44 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
-        $user = auth()->user();
+       
+    // Get the authenticated user (assuming the user is logged in)
+    $user = auth()->user();
 
-        $validated = $request->validate([
-            'name'=>'required',
-            'email' => 'required|email|unique:users,email,'.$user->id.',id',
-            'password' => 'nullable|required_with:password_confirmation|confirmed',
-            'profile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:208',
-        ]);
+    // Validate the input fields from the request
+    $validated = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email,' . $user->id . ',id',
+        'password' => 'nullable|required_with:password_confirmation|confirmed',
+        'profile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:208',
+    ]);
 
-        if($request->filled('password')){
-            $validated['password'] = bcrypt($request->password);
-        }else{
-            // To avoid updating with an empty password.
-            unset($validated['password']);
-        }
-
-        if($request->hasFile('profile')){
-            // Assuming 'saveImage' method works correctly and returns the filename.
-            $name = $this->saveImage($request->file('profile'));
-            $validated['profile'] = $name;
-
-        }
-
-        $user->update($validated);
-
-
-        $notification = array(
-            'message' => 'User Profile Update Successfully',
-            'alert-type' => 'success'
-        );
-
-        return redirect()->route('admin.profile.update')->with($notification);
-
+    // If the password is provided, hash it and add it to the $validated array
+    if ($request->filled('password')) {
+        $validated['password'] = bcrypt($request->password);
+    } else {
+        // To avoid updating with an empty password.
+        unset($validated['password']);
     }
+
+    // If a new profile image is provided, save it and update the 'profile' field
+    if ($request->hasFile('profile')) {
+        // Assuming 'saveImage' method works correctly and returns the filename.
+        $name = $this->saveImage($request->file('profile'));
+        $validated['profile'] = $name;
+    }
+   
+    // Update the user's profile with the validated data
+    $user->update($validated);
+
+    // Prepare a success notification message to be displayed after the update
+    $notification = array(
+        'message' => 'User Profile Update Successfully',
+        'alert-type' => 'success'
+    );
+
+    // Redirect back to the profile update page with the success notification
+    return redirect()->back()->with($notification);
+    }
+
 }
